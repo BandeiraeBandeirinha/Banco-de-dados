cb2f02448b526952932a9d54f97ee23836179f39
BandeiraeBandeirinha <38111222+BandeiraeBandeirinha@users.noreply.github.com>
2018-07-23 15:38:52 -0300

    tb_cliente

---
+++ Avalição2.0.sql
@@ -0,0 +1,53 @@
+use dbinfox;
+-- inspecionar tabelas no banco de dados
+show tables;
+-- o código abaixo cria uma tabela
+create table tb_cliente(
+idcli int primary key,
+cpfcli varchar(50) unique,
+nomecli varchar(50) not null,
+cep varchar(50) not null,
+tipo varchar(50)not null unique,
+logradouro varchar(50) not null,
+numero varchar(50) not null,
+complemento varchar(50),
+bairro varchar(50) not null,
+cidade varchar(50) not null,
+uf varchar(50) not null,
+fone1 varchar(50) not null,
+fone2 varchar(50),
+emailcli varchar(50) not null
+);
+show tables;
+-- descrevendo a tabela
+describe tb_cliente;
+-- inserindo dados na tabela (Create)
+insert into tb_cliente values
+(1,'444.425.658/30','Geovani Matheus Bandeira','08568-300','Avenida Adutora','Adutora','622','oioioi','Jardim Nova Poá','Poá','SP','4638-0052','6969-6969','GMBandeira01@hotmail.com');
+select * from tb_cliente;
+
+use dbinfox;
+-- inspecionar tabelas no banco de dados
+show tables;
+-- o código abaixo cria uma tabela
+create table tb_usuarios(iduser int primary key,
+usuario varchar(50) not null,
+login varchar(50) not null unique,
+senha varchar(50) not null,
+fone varchar(15),
+perfil varchar(50) not null);
+show tables;
+-- descrevendo a tabela
+describe tb_usuarios;
+-- inserindo dados na tabela (Create)
+insert into tb_usuarios values
+(1,'Geovani Matheus Bandeira','gmbandeira','oioioi','91111-1111','Aluno do Senac');
+insert into tb_usuarios values
+(2,'Victor Leite','victorl','victor','92222-2222','Aluno do Senac');
+insert into tb_usuarios values
+(3,'Fabricio Ferreira','fafa','fafa123','93333-3333','Aluno do Senac');
+-- pesquisando dados na tabela (Read)
+select * from tb_usuarios;
+
+
+
